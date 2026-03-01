a9962a8e7826be4eedbb81c61b38162432accf08
Boss_of_pentest228 <magaygregory@gmail.com>
2026-03-01 16:17:28 +0400

    Сделали приближение-отдаление

--- APIProject/main.py
+++ APIProject/main.py
@@ -1,19 +1,24 @@
 import os
 import arcade
 import sys
+
+from pyglet.event import EVENT_HANDLE_STATE
+
 import geocoder
 import requests
 from geocoder import get_spn
-
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "MAP"
 MAP_FILE = "map.png"
 THEMES = ['light', 'dark']
-STANDART_PLACE = 'Москва'
+
+
 class GameView(arcade.Window):
+
     def setup(self):
+        self.default_zoom = 1
         self.get_image()
 
     def on_draw(self):
@@ -29,14 +34,21 @@ class GameView(arcade.Window):
             ),
         )
 
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.PAGEUP:
+            self.default_zoom *= 2
+            self.get_image()
+        if key == arcade.key.PAGEDOWN:
+            self.default_zoom /= 2
+            self.get_image()
+
     def get_image(self):
         toponym_to_find = " ".join(sys.argv[1:])
-        if not toponym_to_find:
-            toponym_to_find = STANDART_PLACE
         toponym = geocoder.get_toponym(toponym_to_find)
         toponym_longitude, toponym_altitude = geocoder.get_coordinates(toponym_to_find)
         if toponym_longitude and toponym_altitude:
-            delta = get_spn(toponym_to_find)
+            delta = str(float(get_spn(toponym_to_find).split(",")[0]) * self.default_zoom) + "," + str(float(get_spn(toponym_to_find).split(",")[1]) * self.default_zoom)
+            print(type(delta))
             apikey = "17efe1e9-c0e1-4c3d-b18c-2cf038f24953"
             ll = ",".join([toponym_longitude, toponym_altitude])
             # Собираем параметры для запроса к StaticMapsAPI:
@@ -55,11 +67,11 @@ class GameView(arcade.Window):
                 print("Http статус:", response.status_code, "(", response.reason, ")")
                 sys.exit(1)
 
-            # Запишем полученное изображение в файл.
-            with open(MAP_FILE, "wb") as file:
-                file.write(response.content)
+        # Запишем полученное изображение в файл.
+        with open(MAP_FILE, "wb") as file:
+            file.write(response.content)
 
-            self.background = arcade.load_texture(MAP_FILE)
+        self.background = arcade.load_texture(MAP_FILE)
 
 
 def main():
