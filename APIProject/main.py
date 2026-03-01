e41f7eb34b351431ca014ca3e91e4e0a2b702e27
Boss_of_pentest228 <magaygregory@gmail.com>
2026-03-01 16:23:51 +0400

    Сделали приближение-отдаление

--- APIProject/main.py
+++ APIProject/main.py
@@ -8,15 +8,14 @@ import geocoder
 import requests
 from geocoder import get_spn
 
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "MAP"
 MAP_FILE = "map.png"
 THEMES = ['light', 'dark']
-
-
+STANDART_PLACE = 'Москва'
 class GameView(arcade.Window):
-
     def setup(self):
         self.default_zoom = 1
         self.get_image()
@@ -44,6 +43,8 @@ class GameView(arcade.Window):
 
     def get_image(self):
         toponym_to_find = " ".join(sys.argv[1:])
+        if not toponym_to_find:
+            toponym_to_find = STANDART_PLACE
         toponym = geocoder.get_toponym(toponym_to_find)
         toponym_longitude, toponym_altitude = geocoder.get_coordinates(toponym_to_find)
         if toponym_longitude and toponym_altitude:
@@ -58,6 +59,7 @@ class GameView(arcade.Window):
                 "apikey": apikey,
                 "theme": "dark"
             }
+            print()
             map_api_server = "https://static-maps.yandex.ru/v1"
             response = requests.get(map_api_server, params=map_params)
 
@@ -66,11 +68,11 @@ class GameView(arcade.Window):
                 print("Http статус:", response.status_code, "(", response.reason, ")")
                 sys.exit(1)
 
-        # Запишем полученное изображение в файл.
-        with open(MAP_FILE, "wb") as file:
-            file.write(response.content)
+            # Запишем полученное изображение в файл.
+            with open(MAP_FILE, "wb") as file:
+                file.write(response.content)
 
-        self.background = arcade.load_texture(MAP_FILE)
+            self.background = arcade.load_texture(MAP_FILE)
 
 
 def main():
